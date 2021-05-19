5f81a847d1cda7c3401546163a6adfd48ee141db
Zihad <zihadnayem8@gmail.com>
2021-05-19 18:30:47 +0600

    learing react component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {
+      'name' : 'Photoshope',
+      'price' : '$56.90'
+    },
+    {
+      'name' : 'Illustetor',
+      'price' : '$60.90'
+    },
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with FUN :)
-        </a>
+       
+ 
+    
+      
+
+<Product name={products[0].name} price={products[0].price}></Product>
       </header>
+
+    </div>
+  );
+}
+
+function Person(props) {
+  const personStyle =  {
+    border : '2px solid cyan',
+    padding: '8px',
+    margin : '5px'
+  }
+  
+  console.log(props)
+  return (
+    <div style={personStyle}>
+      <h5>Name: {props.nayak}</h5>
+      <p>Hero of {props.nayika}</p>
+    </div>
+  );
+}
+
+
+function Product(props){
+  const productStyle =  {
+    border : '1px solid white',
+    padding: '5px',
+    borderRadius:'5px',
+    margin : '5px',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  return (
+    <div style={productStyle}>
+      <h2>Name: {props.name}</h2>
+      <h1>Price: {props.price}</h1>
+      <button>Buy now</button>
     </div>
   );
 }
