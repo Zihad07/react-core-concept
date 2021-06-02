978dfb8283177ddc22f9a05d8075cc667dbd78a6
Zihad <zihadnayem8@gmail.com>
2021-06-02 11:08:03 +0600

    learning basic event handling.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 2,
     tags : ['tag1','tag2','tag3'],
     address: {
       name: 'Dhaka'
     }
+  }
+
+  // Bind event hander by constructor or user arrow function in method
+  // constructor(){
+  //   super()
+  //   // console.log('constructor', this);
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // handleIncrement(){
+  //   console.log('increment', this);
+  // }
+  handleIncrement = () =>{
+    // console.log('increment', this);
+    // this.state.count += 1;
+    this.setState({ count : this.state.count + 1})
+  }
+
+  handleDercrement= () => {
+    // this.state.count -= 1;
+    if(this.state.count != 0){
+      this.setState({ count: this.state.count -1 });
+    }
+
+  }
+  showProduct = ( product )=> {
+    console.log(product)
   }
   render(){
     const today = new Date().toDateString();
 
     return (
       <div className="p-4">
-        <span className={ this.getBadgeClass() }>{ this.formatCount() }</span> <button className="btn btn-sm btn-secondary">Increment</button>
-        <ul>
-          {/* { this.state.tags.map( (tag,index) => <li key={ index }>{ tag }</li>) } */}
-          {this.rederTag()}
-        </ul>
+        <span className={ this.getBadgeClass() }>{ this.formatCount() }</span> 
+        <button className="btn btn-sm btn-secondary mr-2" onClick={ this.handleIncrement }>Increment</button>
+        <button className="btn btn-sm btn-info mr-2" onClick={ this.handleDercrement }>Decrement</button>
+        <button className="btn btn-sm btn-primary" onClick={ ()=> this.showProduct({id:1,name:'Apple'}) }>Show Product</button>
+       
       </div>
     );
+  }
+
+  
+
+  getBadgeClass = () => {
+    let classes = "mr-4 badge badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
   }
 
   rederTag() {
@@ -35,15 +75,19 @@ class Counter extends Component {
     );
   }
 
-  getBadgeClass() {
-    let classes = "mr-4 badge badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
-    return classes;
-  }
+  demoReder() {
+    if(this.state.tags.length === 0) return <p>There are no tag</p>
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return (
+      <ul className="list-group">
+        {
+         this.state.tags.map((item,index)=> 
+           <li className="list-group-item" key={index}>{item}</li>
+         )
+        }
+      </ul>
+
+    );
   }
 }
 
