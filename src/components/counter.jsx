83620430a40ded086158a0c00f6c46c28a081b2e
Zihad <zihadnayem8@gmail.com>
2021-06-08 17:39:17 +0600

    controlling child component from parent component by props.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,49 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    count: 2,
-    tags : ['tag1','tag2','tag3'],
-    address: {
-      name: 'Dhaka'
-    }
-  }
+  
 
-  // Bind event hander by constructor or user arrow function in method
-  // constructor(){
-  //   super()
-  //   // console.log('constructor', this);
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
 
-  // handleIncrement(){
-  //   console.log('increment', this);
-  // }
-  handleIncrement = () =>{
-    // console.log('increment', this);
-    // this.state.count += 1;
-    this.setState({ count : this.state.count + 1})
-  }
 
-  handleDercrement= () => {
-    // this.state.count -= 1;
-    if(this.state.count != 0){
-      this.setState({ count: this.state.count -1 });
-    }
 
-  }
-  showProduct = ( product )=> {
-    console.log(product)
-  }
   render(){
-    const today = new Date().toDateString();
-
+    // const today = new Date().toDateString();
+    // console.log("props :  ", this.props);
     return (
       <div className="p-4">
+        <h4>Counter# {this.props.counters.id}</h4>
         <span className={ this.getBadgeClass() }>{ this.formatCount() }</span> 
-        <button className="btn btn-sm btn-secondary mr-2" onClick={ this.handleIncrement }>Increment</button>
-        <button className="btn btn-sm btn-info mr-2" onClick={ this.handleDercrement }>Decrement</button>
-        <button className="btn btn-sm btn-primary" onClick={ ()=> this.showProduct({id:1,name:'Apple'}) }>Show Product</button>
+        <button className="btn btn-sm btn-secondary mr-2" onClick={ ()=> this.props.onIncrement(this.props.counters) }>Increment</button>
+        <button className="btn btn-sm btn-info mr-2" onClick={()=> this.props.onIncrement(this.props.counters, true)}>Decrement</button>
+        <button className="btn btn-sm btn-danger mx-2" onClick={ ()=> this.props.onDelete(this.props.counters.id)}>Delete</button>
        
       </div>
     );
@@ -53,13 +25,13 @@ class Counter extends Component {
 
   getBadgeClass = () => {
     let classes = "mr-4 badge badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += (this.props.counters.value === 0) ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.props.counters;
+    return value === 0 ? 'Zero' : value;
   }
 
   rederTag() {
--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Counter from './components/counter';
+import Counters from './components/counters';
 import Test from './components/test';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Counter></Counter>
+    <Counters></Counters>
   </React.StrictMode>,
   document.getElementById('root')
 );
